998d2fab5810c328527abf2d004b2d9ab1361e9f
GilsonTrajanoSC <gilsontsc@gmail.com>
2014-07-15 09:30:38 -0300

    Task 1.02 finished for COMPOR

---
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -0,0 +1,23 @@
+package br.ufpb.dce.aps.coffeemachine.impl;
+
+import net.compor.frameworks.jcf.api.ComporFacade;
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
+import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+
+public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine{
+
+	private ComponentsFactory factory;
+	private int divisao, resto;
+	
+	public MyCoffeeMachine(ComponentsFactory factory){
+		this.factory = factory;
+		this.factory.getDisplay().info("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin dime) {
+		this.divisao = dime.getValue() / 100;
+		this.resto = dime.getValue() % 100;
+		this.factory.getDisplay().info("Total: US$ 0.10");
+	}
+}
