8de46aab388a1a6cb9a955eceb91581ee6c5b627
GilsonTrajanoSC <gilsontsc@gmail.com>
2014-07-21 08:43:32 -0300

    Task 1.06 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -5,31 +5,35 @@ import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 
 public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine{
 
 	private ComponentsFactory factory;
-	private int divisao, resto;
+	private int dolares, centavos;
 	
 	public MyCoffeeMachine(ComponentsFactory factory){
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
-		this.divisao = 0;
-		this.resto = 0;
+		this.dolares = 0;
+		this.centavos = 0;
 	}
 
 	public void insertCoin(Coin dime) {
 		if(dime == null){
 			throw new CoffeeMachineException("Coin null");
 		}
-		this.divisao += dime.getValue() / 100;
-		this.resto += dime.getValue() % 100;
-		this.factory.getDisplay().info("Total: US$ " + this.divisao + "." + this.resto);
+		this.dolares += dime.getValue() / 100;
+		this.centavos += dime.getValue() % 100;
+		this.factory.getDisplay().info("Total: US$ " + this.dolares + "." + this.centavos);
 	}
 
 	public void cancel() {
-		if(this.divisao == 0){
+		if(this.dolares == 0 && this.centavos == 0){
 			throw new CoffeeMachineException("Não tem moedas inseridas");
 		}
+		this.factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+		this.factory.getCashBox().release(Coin.halfDollar);
+		this.factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 	}
 }
